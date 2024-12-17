6a9230cc5ba99840a65a70b1f222e79a942eb6f5
Dokjassi <linqianhui258@gmail.com>
2024-12-17 23:23:25 +0000

    FINITOCODICE

--- hello.c
+++ hello.c
@@ -10,55 +10,80 @@ int NOT(int a) {
 int AND(int a, int B) {
     // Il valore di ritorno della funzione
     // TODO aggiungere l'espressione matematica per soddisfare la porta AND
-    return 1337;
+    return a * B;
 }
 
 // Funzione per simulare la porta OR
 int OR(int a, int B) {
     // Il valore di ritorno della funzione
     // TODO aggiungere l'espressione matematica per soddisfare la porta OR
-    return 1337;
+    return a + B - (a*B);
 }
 
-int main() {
-    // Dichiara una variabile
-    int A, B;
 
-    // Mostra un messaggio sul terminale che l'utente può leggere
-    printf("Inserisci il valore di A (0 o 1):\n");
-    // Attende che l'utente inserisca un valore e prema invio
+
+int main
+() {
+
+int A,B,C,D,E,F;
+
+printf("Inserisci il valore di A (0 o 1):\n");
     scanf("%d", &A);
-    // Mostra un messaggio sul terminale che l'utente può leggere
-    printf("Hai inserito il valore A: %d\n", A);
-
-    // Mostra un messaggio sul terminale che l'utente può leggere
-    printf("Inserisci il valore di B (0 o 1):\n");
-    // Attende che l'utente inserisca un valore e prema invio
+printf("Inserisci il valore di B (0 o 1):\n");
     scanf("%d", &B);
-    // Mostra un messaggio sul terminale che l'utente può leggere
-    printf("Hai inserito il valore B: %d\n", B);
+printf("Inserisci il valore di C (0 o 1):\n");
+    scanf("%d", &C);
+printf("Inserisci il valore di D (0 o 1):\n");
+    scanf("%d", &D);
+printf("Inserisci il valore di E (0 o 1):\n");
+    scanf("%d", &E);
+printf("Inserisci il valore di F (0 o 1):\n");
+    scanf("%d", &F);
+     if (A != 0 && A != 1 && B != 0 && B != 1 && C != 0 && C != 1 && D != 0 && D != 1 && E != 0 && E != 1 && F != 0 && F != 1) {
+        printf("I valori inseriti non sono 1 o 0\n");}
 
-    if (A != 0 && A != 1 && B != 0 && B != 1) {
-        printf("I valori inseriti non sono 1 o 0\n");
-    } else {
-        // Procediamo con l'esecuzione solo se il numero è 1 o 0
-        printf("I valori inseriti sono 1 o 0\n");
-
-        // Esempio di porta NOT
         printf("Il valore di A viene trasformato da una porta NOT\n");
         int notA = NOT(A);
         printf("Il valore di uscita della porta NOT è: %d\n", notA);
 
-        // Esempio di porta AND
-        printf("I valori di A e B vengono trasformati da una porta AND\n");
-        int andAB = AND(A, B);
-        printf("Il valore di uscita della porta AND è: %d\n", andAB);
+          printf("I valori di A e notA vengono trasformati da una porta OR\n");
+        int orAnotA = OR(A, notA);
+        printf("Il valore di uscita della porta OR è: %d\n", orAnotA);
 
-        // Esempio di porta OR
-        printf("I valori di A e B vengono trasformati da una porta OR\n");
-        int orAB = OR(A, B);
-        printf("Il valore di uscita della porta OR è: %d\n", orAB);
-    }
+        printf("Il valore di B viene trasformato da una porta NOT\n");
+        int notB = NOT(B);
+        printf("Il valore di uscita della porta NOT è: %d\n", notB);
 
-    printf("\n\n");
-}
+          printf("I valori di B e notB vengono trasformati da una porta OR\n");
+        int orBnotB = OR(B, notB);
+        printf("Il valore di uscita della porta OR è: %d\n", orBnotB);
+
+         printf("I valori di orAnotA e orBnotB vengono trasformati da una porta AND\n");
+        int andorAnotAorBnotB = AND(orAnotA, orBnotB);
+        printf("Il valore di uscita della porta AND è: %d\n", andorAnotAorBnotB);
+
+        printf("I valori di andorAnotAorBnotB e C vengono trasformati da una porta OR\n");
+        int orandorAnotAorBnotBC = OR(andorAnotAorBnotB, C);
+        printf("Il valore di uscita della porta OR è: %d\n", orandorAnotAorBnotBC);
+
+        printf("I valori di andorAnotAorBnotBC e D vengono trasformati da una porta OR\n");
+        int ororandorAnotAorBnotBCD = OR(orandorAnotAorBnotBC, D);
+        printf("Il valore di uscita della porta OR è: %d\n", ororandorAnotAorBnotBCD);
+
+        printf("I valori di andorAnotAorBnotBCD e E vengono trasformati da una porta OR\n");
+        int orororandorAnotAorBnotBCDE = OR(ororandorAnotAorBnotBCD, E);
+        printf("Il valore di uscita della porta OR è: %d\n", orororandorAnotAorBnotBCDE);
+        
+         printf("I valori di andorAnotAorBnotBCDE e F vengono trasformati da una porta OR\n");
+        int ororororandorAnotAorBnotBCDEF = OR(orororandorAnotAorBnotBCDE, F);
+        printf("Il valore di uscita della porta OR è: %d\n", ororororandorAnotAorBnotBCDEF);
+
+     }
+
+
+    
+
+
+
+
+
